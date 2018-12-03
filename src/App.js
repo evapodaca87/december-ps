c5578ef75e8752daf305c279fd467195f833169e
evapodaca87 <evapodaca87@gmail.com>
2018-12-03 16:33:05 -0700

    added menu bar from semantic ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Parallax1 from './Parallax.js';
+import { Menu } from 'semantic-ui-react';
+import Menu1 from './nav';
 
 class App extends Component {
     render() {
@@ -8,6 +10,7 @@ class App extends Component {
             <div className='App'>
                 <div className='nav'>
                     <h1>Evan Apodaca</h1>
+                    <Menu1 />
                 </div>
                 <div className='content'>
                     <Parallax1 />
