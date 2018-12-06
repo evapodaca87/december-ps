2558251bd336658f6e09c8d808365f14cb022031
evapodaca87 <evapodaca87@gmail.com>
2018-12-06 11:43:51 -0700

    added contacts

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import './App.css';
 
-const Contact = () => (
-    <div className='contact'>
-        <h1>Contact</h1>
-
-
-    
-       
-    </div>
-);
+const Contact = () => {
+    return (
+        <div className='contacts'>
+            <a href='https://www.linkedin.com/in/evan-apodaca/'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/25/25320.svg' />
+            </a>
+            <a href='EMAIL'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/37/37572.svg' />
+            </a>
+            <a href='https://github.com/evapodaca87'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/25/25657.svg' />
+            </a>
+            <a href='https://talent.galvanize.com/students/3122'>
+                <img className='link-badge' src='http://i63.tinypic.com/2ujgzk4.png' />
+            </a>
+            <a href='https://drive.google.com/file/d/19025nKMmW7f2rdEjvwWVxmVOJ37GEYwb/view?usp=sharing'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1244/1244742.svg' />
+            </a>
+            <a href='https://open.spotify.com/artist/0jVctYQtkzafC6FoyPqacD'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/49/49097.svg' />
+            </a>
+            <a href='HOME'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/70/70370.svg' />
+            </a>
+        </div>
+    );
+};
 export default Contact;
