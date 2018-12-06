260f4416ce4c8a5e37ae074b18cea0e13e743233
evapodaca87 <evapodaca87@gmail.com>
2018-12-06 15:01:43 -0700

    added music content

--- src/music.js
+++ src/music.js
@@ -2,7 +2,124 @@ import React from 'react';
 
 const Music = () => (
     <div className='contact'>
-        <h1>Music</h1>
+        <div className='music'>
+            <a href='https://open.spotify.com/artist/0jVctYQtkzafC6FoyPqacD'>
+                <figure className='snip1579'>
+                    <img
+                        className='boxes'
+                        src='https://images-na.ssl-images-amazon.com/images/I/51-uFt5ueUL._SS500.jpg'
+                        alt='profile-sample2'
+                    />
+                    <figcaption>
+                        <h3>15.10</h3>
+                        <h5>Album</h5>
+                        <blockquote>
+                            <p>My first and only solo album available on most platforms</p>
+                        </blockquote>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href='https://www.youtube.com/channel/UCkTpCNPkxwrHfl0kNVgkGxQ'>
+                <figure className='snip1579'>
+                    <img
+                        className='boxes'
+                        src='https://yt3.ggpht.com/a-/AN66SAwz1j3RU6ckMxR_IMmBN2i8LkA2VucH0pjJMw=s288-mo-c-c0xffffffff-rj-k-no'
+                        alt='profile'
+                    />
+                    <figcaption>
+                        <h3>Simply Sampled</h3>
+                        <h5>YouTube Channel</h5>
+                        <blockquote>
+                            <p>
+                                A YouTube channel I created with a producer friend where we are given one song to sample
+                                and have to some up with a 1-minute beat/song out of it. Started as a way to insire
+                                ourselves to make more music.
+                            </p>
+                        </blockquote>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href='https://soundcloud.com/inflatedpenguins'>
+                <figure className='snip1579'>
+                    <img
+                        className='boxes'
+                        src='https://i1.sndcdn.com/avatars-000490337808-6pa1iv-t500x500.jpg'
+                        alt='pixels'
+                    />
+                    <figcaption>
+                        <h3>Inflated Penguins</h3>
+                        <h5>Soundcloud</h5>
+                        <blockquote>
+                            <p>More of my songs, beats, and remixes not featured on my album.</p>
+                        </blockquote>
+                    </figcaption>
+                </figure>
+            </a>
+            <hr />
+            <br />
+            <h3>Here are a few of my latest or favorite songs:</h3>
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/277152556&color=%2312161d&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/491184321&color=%2339444e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/302441399&color=%23302c14&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/285744020&color=%231b250e&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/164610401&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/216503209&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/160318917&color=%23171716&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <h3>I'm also a DJ in Denver, here are a few of my favorite mixes:</h3>
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/487401645&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/450019875&color=%231b1321&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+            <iframe
+                className='snip1579'
+                scrolling='yes'
+                frameborder='no'
+                src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/309323991&color=%2312101a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+            />
+        </div>
 
 
     
