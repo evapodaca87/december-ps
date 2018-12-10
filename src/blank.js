cfa708259d88d7e8ffa068a04341d8ae8ad3464f
evapodaca87 <evapodaca87@gmail.com>
2018-12-10 16:07:05 -0700

    update to responsive queries

--- src/blank.js
+++ src/blank.js
@@ -4,25 +4,20 @@ import './images/inflated.JPG';
 
 const Blank = () => (
     <div className='blank1'>
-        
         <div className='home'>
-        <h1 className="big">Evan Apodaca</h1>
-        {/* -----basic config-----*/}
-        <Parallax
-            className='parallax2'
-            blur={0}
-            bgImage={require('./images/inflated.JPG')}
-            bgImageAlt='the cat'
-            strength={100}
-        >
-            
-            <div style={{ width: '90vw', height: '90vh' }} />
-        </Parallax>
-        <div className='p2' />
-    </div>
-
-    
-       
+            <h1 className='big'>Evan Apodaca</h1>
+            {/* -----basic config-----*/}
+            <Parallax
+                className='parallax2'
+                blur={0}
+                bgImage={require('./images/inflated.JPG')}
+                bgImageAlt='the cat'
+                strength={100}
+            >
+                <div style={{ width: '90vw', height: '90vh' }} />
+            </Parallax>
+            <div className='p2' />
+        </div>
     </div>
 );
 export default Blank;
